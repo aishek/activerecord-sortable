02124926c912739e3a92e3c95376fbc7529cddf4
Alexandr Borisov <aishek@gmail.com>
2015-01-07 20:33:12 +0300

    hotfixed specs

--- spec/dummy_rails_app.rb
+++ spec/dummy_rails_app.rb
@@ -10,11 +10,19 @@ environment_paths = version_parts.length.downto(1).map do |count|
   File.expand_path("../dummy-#{version_part}/config/environment.rb", __FILE__)
 end
 
+app_path = "spec/dummy-#{version_parts.take(2).join('.')}"
+
 # Require environment if any dummy app exists, otherwise abort with instructions
 if (environment_path = environment_paths.find(&File.method(:exist?)))
   require environment_path
+
+  ActiveRecord::Base.establish_connection(:adapter => 'sqlite3', :database => ':memory:')
+
+  schema_path = "#{app_path}/db/schema.rb"
+  load(schema_path)
+
+  require 'activerecord_shared_db_connection_patch'
 else
-  app_path = "spec/dummy-#{version_parts.take(2).join('.')}"
 
   rails_command = "rails new #{app_path} -TSJ --skip-bundle"
   command = "RAILS_VERSION=#{version} bundle exec #{rails_command}"
